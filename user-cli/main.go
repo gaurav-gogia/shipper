5df964bb25a749b5abc240a9715852bb5e34d588
Gaurav Gogia <desmondmiles36@gmail.com>
2018-06-23 19:51:52 +0530

    user-cli added

--- user-cli/main.go
+++ user-cli/main.go
@@ -1,9 +1,81 @@
 package main
 
 import (
+	"context"
+	"log"
+	"os"
+
+	pb "github.com/DesmondANIMUS/shipper/user-service/proto/user"
+	"github.com/micro/cli"
+	micro "github.com/micro/go-micro"
+	microclient "github.com/micro/go-micro/client"
 	"github.com/micro/go-micro/cmd"
+)
+
+const (
+	sname = "go.micro.srv.user"
 )
 
 func main() {
 	cmd.Init()
+	client := pb.NewUserServiceClient(sname, microclient.DefaultClient)
+
+	service := micro.NewService(
+		micro.Flags(
+			cli.StringFlag{
+				Name:  "name",
+				Usage: "You full name",
+			},
+			cli.StringFlag{
+				Name:  "email",
+				Usage: "Your email",
+			},
+			cli.StringFlag{
+				Name:  "password",
+				Usage: "Your password",
+			},
+			cli.StringFlag{
+				Name:  "company",
+				Usage: "Your company",
+			},
+		),
+	)
+
+	service.Init(
+
+		micro.Action(func(c *cli.Context) {
+
+			name := c.String("name")
+			email := c.String("email")
+			password := c.String("password")
+			company := c.String("company")
+
+			// Call our user service
+			r, err := client.Create(context.TODO(), &pb.User{
+				Name:     name,
+				Email:    email,
+				Password: password,
+				Company:  company,
+			})
+			if err != nil {
+				log.Fatalf("Could not create: %v", err)
+			}
+			log.Printf("Created: %s", r.User.Id)
+
+			getAll, err := client.GetAll(context.Background(), &pb.Request{})
+			if err != nil {
+				log.Fatalf("Could not list users: %v", err)
+			}
+			for _, v := range getAll.Users {
+				log.Println(v)
+			}
+
+			os.Exit(0)
+		}),
+	)
+
+	// Run the server
+	if err := service.Run(); err != nil {
+		log.Println(err)
+	}
 }
